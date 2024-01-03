33844438e6c5b5c74e345b68a5d06ccfd24cdd1c
ragog <giovanni@checklyhq.com>
2024-01-03 22:48:09 +0100

    refactor: naming

--- src/__checks__/xs2a.check.ts
+++ src/__checks__/xs2a.check.ts
@@ -2,11 +2,11 @@ import * as path from 'path'
 import { MultiStepCheck } from 'checkly/constructs'
 import { emailChannel, callChannel } from './alertChannels'
 
-new MultiStepCheck('homepage-browser-check', {
+new MultiStepCheck('xs2a-flow-check', {
     name: 'Klarna Open Banking - XS2A API Flow',
     alertChannels: [emailChannel, callChannel],
     muted: false,
     code: {
-      entrypoint: path.join(__dirname, 'multistep.spec.ts')
+      entrypoint: path.join(__dirname, 'xs2a.spec.ts')
     },
   })
